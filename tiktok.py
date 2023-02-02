f638e8b73580883e6dc51942aa0515ade5c421f9
aimen08 <aimen_hamza@hotmail.com>
2023-02-02 16:57:48 +0100

    fixed error

--- tiktok.py
+++ tiktok.py
@@ -30,15 +30,21 @@ def snap(message):
     if len(request) > 1 :
         bot.send_message(message.chat.id, "الرجاء التأكد من صحة الربط")
         return 
-                       
-    with TikTokApi() as api:
-        bot.send_message(message.chat.id, "جاري التحميل يرجى إنتظار")       
-        video=  api.video(url=message.text)
-        logger.info("[+] {} has video with id {} ".format(video.info()["author"]["nickname"],video.info()["id"] ))
+    
+    while True:
+        try:
+            with TikTokApi() as api:
+                bot.send_message(message.chat.id, "جاري التحميل يرجى إنتظار")       
+                video=  api.video(url=message.text)
+                logger.info("[+] {} has video with id {} ".format(video.info()["author"]["nickname"],video.info()["id"] ))
 
-        bot.send_video(message.chat.id,video.bytes())
-        bot.send_message(message.chat.id, "تم تحميل")   
-        logger.info("[✔] {} stories downloaded".format(video.info()["author"]["nickname"]))
+                bot.send_video(message.chat.id,video.bytes())
+                bot.send_message(message.chat.id, "تم تحميل")   
+                logger.info("[✔] {} video downloaded".format(video.info()["author"]["nickname"]))
+        except:
+            continue
+        break         
+   
         
 
     
